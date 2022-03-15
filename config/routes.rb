e27c76789625091f9b194006731d35ff8280b63b
Mark Murillo <mmurillo@academic.rrc.ca>
2022-03-15 17:46:13 -0500

    Add Bulma styling

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
+  get '/customers/alphabetized', to: "customers#alphabetized"
+  get '/customers/missing_email', to: "customers#missing_email"
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root "customers#index"
 end
